9ff0d09950dd4cd54382237915c2cdae6c09594a
xxw <xxw@qq.com>
2024-08-10 22:01:37 +0800

    feat#104二叉树的最大深度

---
+++ 二叉树/104二叉树的最大深度.js
@@ -0,0 +1,41 @@
+// 给定一个二叉树 root ，返回其最大深度。
+
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+// 输入：root = [3,9,20,null,null,15,7]
+// 输出：3
+// 示例 2：
+
+// 输入：root = [1,null,2]
+// 输出：2
+
+
+// 提示：
+
+// 树中节点的数量在 [0, 104] 区间内。
+// -100 <= Node.val <= 100
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    // 全局变量
+    let res = 0
+    if (root === null) {
+        return 0
+    } else {
+        let left = maxDepth(root.left)
+        let right = maxDepth(root.right)
+        return Math.max(left, right) + 1
+    }
+};
